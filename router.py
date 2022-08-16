8664ddd36010c7cba106c271fa48b1843b61fdf1
MashinaSSokom <skltan@mail.ru>
2022-08-16 20:21:03 +0300

    temporarily removed some urls in router

--- router.py
+++ router.py
@@ -3,6 +3,6 @@ from views import main_view, contacts_view, login_view, registration_view
 routes = {
     '/': main_view,
     '/contacts': contacts_view,
-    '/login': login_view,
-    '/registration': registration_view
+    # '/login': login_view,
+    # '/registration': registration_view
 }
